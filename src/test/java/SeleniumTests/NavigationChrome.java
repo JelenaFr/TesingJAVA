b9cfe7c3b50a5a92ded3075052bc253d010c98c1
JelenaFr <jelena.freiberg@gmail.com>
2021-03-11 23:51:48 +0200

    Assignment_5_folder Selenium

---
+++ src/test/java/SeleniumTests/NavigationChrome.java
@@ -0,0 +1,80 @@
+package SeleniumTests;
+
+import Selenium.SomePage;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Order;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class NavigationChrome {
+
+    private WebDriver driver;
+    private SomePage somePage;
+
+
+    @Before
+    public void SetUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Yelena\\Documents\\Diploma\\testingLessons\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.w3schools.com/html/html_tables.asp");
+        somePage = new SomePage();
+    }
+
+    @Test
+    @Order(1)
+    public void WebDriverStart() {
+        Assert.assertEquals("HTML Tables", driver.getTitle());
+        System.out.println("Page title is : " + driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+    }
+
+    @Test
+    @Order(3)
+    public void TableTestByElementText() {
+        //driver.findElement(By.id("edit-search-block-form--2")).sendKeys("Narva College");
+        WebElement tableElement = driver.findElement(By.xpath("//*[@id=\"customers\"]/tbody/tr[3]/td[1]"));
+        Assert.assertEquals("Centro comercial Moctezuma", tableElement.getText());
+        System.out.println("Info : " + tableElement.getText());
+        driver.quit();
+
+    }
+
+    @Test
+    @Order(2)
+    public void NextButtonTest() {
+        //driver.navigate().to("https://www.w3schools.com/html/html_tables.asp");
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/a[2]"));
+        Assert.assertEquals("Next ❯", button.getText());
+        button.click();
+        String url = driver.getCurrentUrl();
+        Assert.assertEquals("https://www.w3schools.com/html/html_lists.asp", url);
+    }
+
+
+    @Test
+    @Order(4)
+    public void TableTestByPosition() {
+        WebElement tableElement = driver.findElement(By.xpath("//*[@id=\"customers\"]"));
+        Table table = new Table(tableElement, driver);
+        Assert.assertEquals("Austria", table.getValueFromCell(3, 3));
+        System.out.println("Info : " + table.getValueFromCell(3, 3));
+        Assert.assertEquals("Francisco Chang", table.getValueFromCell(2, "Contact"));
+        System.out.println("Info : " + table.getValueFromCell(2, "Contact"));
+
+    }
+
+    @After
+    public void testDown() {
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/SeleniumTests/NavigationFirefox.java
@@ -0,0 +1,37 @@
+package SeleniumTests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Order;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class NavigationFirefox {
+    static WebDriver driver;
+    @Test
+    @Order(1)
+    public void WebDriverStart() {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Yelena\\Documents\\Diploma\\testingLessons\\drivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.get("https://www.ut.ee/et");
+        Assert.assertEquals("Tartu Ülikool", driver.getTitle());
+        System.out.println("Page title is : " +driver.getTitle());
+    }
+
+    @Test
+    @Order(2)
+    public void WebDriverElement() {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Yelena\\Documents\\Diploma\\testingLessons\\drivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.get("https://www.ut.ee/et");
+        //driver.findElement(By.id("edit-search-block-form--2")).sendKeys("Narva College");
+        WebElement textBox =driver.findElement(By.id("edit-search-block-form--2"));
+        driver.findElement(By.className("pane-content"));
+        textBox.sendKeys("Narva College"+ Keys.ENTER);
+    }
+
+}
