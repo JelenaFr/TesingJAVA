4e3ec7640078369c7dc30da8c04a747e3a09dd9f
JelenaFr <jelena.freiberg@gmail.com>
2021-03-21 20:49:59 +0200

    Assignment_6

---
+++ src/test/java/SeleniumAssignment_6/RegistrationPage.java
@@ -0,0 +1,43 @@
+package SeleniumAssignment_6;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.sql.Driver;
+
+public class RegistrationPage {
+
+    private WebDriver driver;
+
+    public RegistrationPage(WebDriver driver) {
+    }
+
+    private By facebookLinkField = By.xpath("//*[@id=\"react-root\"]/section/main/div/div/div[1]/div/form/div[1]/button");
+    private By mobileOrEmailField = By.cssSelector("[aria-label='Mobile Number or Email']");
+    private By fullnameField = By.cssSelector("[aria-label='Full Name']");
+    private By userNameField = By.cssSelector("[aria-label='Username']");
+    private By passwordField = By.cssSelector("[aria-label='Password']");
+    private By nextButtonField = By.cssSelector("[type='submit']");
+    private By alertAccept = By.xpath("[class*=bIiDR]");
+
+
+
+    public RegistrationPage createOrRegisterAccount(String mobileOrEmail, String fullname, String userName, String password, WebDriver driver, Boolean register) {
+        driver.findElement(mobileOrEmailField).sendKeys(mobileOrEmail);
+        driver.findElement(fullnameField).sendKeys(fullname);
+        driver.findElement(userNameField).sendKeys(userName);
+        driver.findElement(passwordField).sendKeys(password);
+        if (register) {
+            driver.findElement(nextButtonField).click();
+        } else {
+            driver.findElement(nextButtonField).isEnabled();
+        }
+        return new RegistrationPage(driver);
+    }
+
+    public void facebookRegistration(WebDriver driver) {
+        driver.findElement(By.cssSelector("[class*=bIiDR]")).click();
+        driver.findElement(facebookLinkField).click();
+
+    }
+}
---
+++ src/test/java/SeleniumAssignment_6/RegistrationPageTest.java
@@ -0,0 +1,108 @@
+package SeleniumAssignment_6;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Order;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class RegistrationPageTest {
+    private WebDriver driver;
+    private RegistrationPage registrationPage;
+
+    @Before
+    public void SetUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Yelena\\Documents\\Diploma\\testingLessons\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("https://www.instagram.com/accounts/emailsignup/");
+        registrationPage = new RegistrationPage(driver);
+
+    }
+
+    @Test
+    @Order(1)
+    public void createAccountTest() {
+        driver.findElement(By.cssSelector("[class*=bIiDR]")).click();
+        driver.getCurrentUrl();
+        registrationPage.createOrRegisterAccount("jelenafreiberg@hot.ee",
+                "Jelena Freiberg", "JFSeleniumTest", "maiuspala", driver, false);
+
+
+    }
+
+    @Test
+    @Order(2)
+    public void facebookRegistrationTest() {
+        registrationPage.facebookRegistration(driver);
+        Assert.assertTrue(driver.getCurrentUrl().contains("https://www.facebook.com/login.php"));
+
+    }
+
+    @Test
+    public void termsLinkTest() {
+        driver.findElement(By.cssSelector("[class*=bIiDR]")).click();
+        String loginButton = "[href=\"/accounts/login/?source=auth_switcher\"]";
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", driver.findElement(By.cssSelector(loginButton)));
+        Assert.assertTrue(driver.getCurrentUrl().contains("https://www.instagram.com/accounts/login/?source=auth_switcher"));
+    }
+
+    @Test
+    public void alertMessagesValidEmailTest(){
+        driver.findElement(By.cssSelector("[class*=bIiDR]")).click();
+        registrationPage.createOrRegisterAccount("jelenafreiberghot.ee",
+                "Jelena Freiberg", "JFSeleniumTest", "maiuspala", driver, true);
+        Assert.assertEquals(driver.findElement(By.cssSelector("[role='alert']")).getText(), "Enter a valid email address.");
+
+
+    }
+    @Test
+    public void alertMessagesValidPhoneTest(){
+        driver.findElement(By.cssSelector("[class*=bIiDR]")).click();
+        //driver.getCurrentUrl();
+        registrationPage.createOrRegisterAccount("372556847",
+                "Jelena Freiberg", "JFSeleniumTest", "maiuspala", driver, true);
+        Assert.assertEquals(driver.findElement(By.cssSelector("[role='alert']")).getText(), "Looks like your phone number may be incorrect. Please try entering your full number, including the country code.");
+
+
+    }
+
+    @Test ()
+    //This method always failed. Instagram has not alert for this situation
+    public void alertMessagesValidNameTest(){
+        driver.findElement(By.cssSelector("[class*=bIiDR]")).click();
+        registrationPage.createOrRegisterAccount("jelenafreiberg@hot.ee",
+                "**********", "JFSeleniumTest", "maiuspala", driver, true);
+        Assert.assertEquals(driver.findElement(By.cssSelector("[role='alert']")).getText(), "Usernames can only use letters, numbers, underscores and periods.");
+    }
+    @Test ()
+    public void alertMessagesUniqueNameTest(){
+        driver.findElement(By.cssSelector("[class*=bIiDR]")).click();
+        registrationPage.createOrRegisterAccount("jelenafreiberg@hot.ee",
+                "Jelena Freiberg", "melaniatrump", "maiuspala", driver, true);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("[role='alert']")).getText(), "This username isn't available. Please try another.");
+    }
+
+    @Test
+    public void alertMessagesPasswordTest(){
+        driver.findElement(By.cssSelector("[class*=bIiDR]")).click();
+        registrationPage.createOrRegisterAccount("jelenafreiberg@hot.ee",
+                "Jelena Freiberg", "JFSeleniumTest", "123456", driver, true);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("[role='alert']")).getText(), "This password is too easy to guess. Please create a new one.");
+    }
+
+
+    @After
+    public void testDown() {
+        driver.quit();
+    }
+
+}
